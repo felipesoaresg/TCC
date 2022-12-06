436405c3b380a255b922edf00711ec11aff6c7d0
felipe soares <100864941+felipesoaresg@users.noreply.github.com>
2022-12-06 20:20:53 -0300

    Update script.js

--- pagamento/JS/script.js
+++ pagamento/JS/script.js
@@ -18,14 +18,22 @@ function marcaDesmarca(id) {
   }
 }
 
-function enviar() {
+let enviar=document.getElementById("enviar")
+let pagamentoSelecionado=[]
+function enviarpag(){
   const url = 'http://10.0.0.107:1313/enviar-pedido'
+  
+  let pagamento = document.getElementsByName("pagamento")
 
-  var pagamento = document.querySelectorAll('[name=pagamento]:checked');
-
-  var meusDados = {
-      pagamento,
+  for(var i=0;i<pagamento.length;i++){
+    if(pagamento[i].checked){
+      console.log("a forma de pagamento foi:  "+ pagamento[i].value)
+      pagamentoSelecionado.push(pagamento[i].value)
+    }
   }
+    var meusDados = {
+      pagamento,
+    }
   fetch(url, {
       method: 'Post',
       body: JSON.stringify(meusDados),
@@ -36,3 +44,5 @@ function enviar() {
       return resultado.json();
   })
 }
+
+enviar.addEventListener("click", enviarpag)
